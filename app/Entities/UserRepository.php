42ed3024aa82f879e4e6f05640feb07d4a3feb98
Klebson Mateus <klebsonbm@gmail.com>
2016-05-16 04:11:10 +0000

    CRUD Projects quase pronto, falta só exibir os ID de client e owner

--- app/Entities/UserRepository.php
+++ app/Entities/UserRepository.php
@@ -6,17 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
 
-class Project extends Model implements Transformable
+class UserRepository extends Model implements Transformable
 {
     use TransformableTrait;
 
-    protected $fillable = [
-	'owner_id',
-	'client_id',
-	'name',
-	'description',
-	'progress',
-	'status',
-    ];
+    protected $fillable = [];
 
 }
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use Illuminate\Http\Request;
+use CodeProject\Repositories\ProjectRepository;
+use CodeProject\Services\ProjectService;
+
+
+class UserController extends Controller
+{
+    /**
+     * @var ProjectRepository
+     */
+    private $repository;
+
+    /**
+     * @var ProjectService
+     */
+    private $service;
+
+    /**
+     * @param ProjectRepository $repository
+     * @param ProjectService $service
+     */
+    public function __construct(UserRepository $repository, UserService $service)
+    {
+        $this->repository = $repository;
+        $this->service = $service;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return $this->repository->all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        return $this->service->create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return $this->repository->find($id);
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return $this->service->update($request->all(), $id);
+               
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $registro = $this->repository->find($id);
+        $this->repository->delete($id);
+        return "O usuário ". $registro->name . " foi deletado com sucesso";
+
+    }
+}
