70c4d8f7b963abe293959b7a3802fc787a5ac2c2
Klebson Mateus <klebsonbm@gmail.com>
2016-05-18 01:26:45 +0000

    Configurando OAuth2

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -12,12 +12,13 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \CodeProject\Http\Middleware\EncryptCookies::class,
-        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        
+    \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+    \CodeProject\Http\Middleware\EncryptCookies::class,
+    \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+    \Illuminate\Session\Middleware\StartSession::class,
+    \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+    \LucaDegasperi\OAuth2Server\Middleware\OAuthExceptionHandlerMiddleware::class,
+    
     ];
 
     /**
@@ -26,9 +27,13 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth' => \CodeProject\Http\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'guest' => \CodeProject\Http\Middleware\RedirectIfAuthenticated::class,
-        'csrf' => \CodeProject\Http\Middleware\VerifyCsrfToken::class,
+    'auth' => \CodeProject\Http\Middleware\Authenticate::class,
+    'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+    'guest' => \CodeProject\Http\Middleware\RedirectIfAuthenticated::class,
+    'csrf' => \CodeProject\Http\Middleware\VerifyCsrfToken::class,
+    'oauth' => \LucaDegasperi\OAuth2Server\Middleware\OAuthMiddleware::class,
+    'oauth-user' => \LucaDegasperi\OAuth2Server\Middleware\OAuthUserOwnerMiddleware::class,
+    'oauth-client' => \LucaDegasperi\OAuth2Server\Middleware\OAuthClientOwnerMiddleware::class,
+    'check-authorization-params' => \LucaDegasperi\OAuth2Server\Middleware\CheckAuthCodeRequestMiddleware::class,
     ];
 }
