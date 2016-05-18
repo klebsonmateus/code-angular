1f2fe4352121d5abd35692f271b941bb6aed7769
Klebson Mateus <klebsonbm@gmail.com>
2016-05-18 02:46:34 +0000

    Agrupando Rotas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,25 +26,30 @@ Route::get('user/{id}', 'UserController@show');
 Route::delete('user/{id}', 'UserController@destroy');
 Route::put('user/{id}', 'UserController@update');
 
+Route::group(['middleware'=>'oauth'], function(){
 
-//Clients
-Route::get('client', ['middleware' => 'oauth','uses'=>'ClientController@index']);
-Route::post('client', 'ClientController@store');
-Route::get('client/{id}', 'ClientController@show');
-Route::delete('client/{id}', 'ClientController@destroy');
-Route::put('client/{id}', 'ClientController@update');
 
-//ProjectNotes
-Route::get('project/{id}/note', 'ProjectNoteController@index');
-Route::post('project/{id}/note', 'ProjectNoteController@store');
-Route::get('project/{id}/note/{noteId}', 'ProjectNoteController@show');
-Route::put('project/{id}/note/{noteId}', 'ProjectNoteController@update');
-Route::delete('project/{id}/note/{noteId}', 'ProjectNoteController@destroy');
 
-//Projects
-Route::get('project', 'ProjectController@index');
-Route::post('project', 'ProjectController@store');
-Route::get('project/{id}', 'ProjectController@show');
-Route::delete('project/{id}', 'ProjectController@destroy');
-Route::put('project/{id}', 'ProjectController@update');
+	//Clients
+	Route::resource('client','ClientController',['except'=>['create','edit']]);
+
+	Route::group(['prefix'=>'project'],function() {
+		//Projects
+		Route::resource('','ProjectController',['except'=>['create','edit']]);
+
+		//ProjectNotes
+		Route::get('/{id}/note', 'ProjectNoteController@index');
+		Route::post('/{id}/note', 'ProjectNoteController@store');
+		Route::get('/{id}/note/{noteId}', 'ProjectNoteController@show');
+		Route::put('/{id}/note/{noteId}', 'ProjectNoteController@update');
+		Route::delete('/{id}/note/{noteId}', 'ProjectNoteController@destroy');
+	});
+
+
+	
+
+
+});
+
+
 
